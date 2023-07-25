d0d306607c319abb0b265f16d4bc7162be54ad41
Raghav Gupta <raghav.gupta13@outlook.com>
2023-07-25 10:54:45 -0700

    Updates 2 files for use case

--- app/src/main/java/com/example/atmos/A.java
+++ app/src/main/java/com/example/atmos/A.java
@@ -3,5 +3,5 @@ package com.example.atmos;
 public class A {
 
     //This is a test class
-    //This is being created to test the reset
+
 }
--- app/src/main/java/com/example/atmos/B.java
+++ app/src/main/java/com/example/atmos/B.java
@@ -3,5 +3,5 @@ package com.example.atmos;
 public class B {
 
     //This is a test file
-    //This is created to resolve reset issues
+
 }
